e7c1ea725b1dbc28e3073ff2092b4eed00c853ce
Hamid Sahraye <hsahraye@sfsu.edu>
2025-03-19 00:51:26 -0700

    fix: add basic structure to CategoryClient component

--- app/resources/[category]/CategoryClient.tsx
+++ app/resources/[category]/CategoryClient.tsx
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+
+const CategoryClient = () => {
+  return (
+    <div>
+      <h1>Category Client</h1>
+      <p>This is a placeholder component for CategoryClient.</p>
+    </div>
+  );
+};
+
+export default CategoryClient; 
